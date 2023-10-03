89eb8ea2ce518ea3df21166fbf3d2c74d2e93a16
Zygintas Gudabskas <zygis@Zygintass-MBP.lan>
2023-10-03 09:03:27 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,32 +1,25 @@
-import './App.css'
-// import {BrowserRouter, Routes, Route, useNavigate} from "react-router-dom";
-// import {useDispatch, useSelector} from "react-redux";
-// import {useState, useEffect} from "react"
-import {BrowserRouter, Routes, Route} from "react-router-dom";
-import ChoseFigure from "./pages/ChoseFigure";
-import GameBoard from "./pages/GameBoard";
-
+import './App.css';
+import AddUserForm from "./pages/AddUserForm";
+import Login from "./pages/Login";
+import { useSelector } from "react-redux";
+import UserProfile from "./pages/UserProfile";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
+    const user = useSelector(state => state.user.user);
 
+    return (
+        <div className="main">
+            <BrowserRouter>
 
-
-return (
-    <div>
-
-        <BrowserRouter>
-
-            <Routes>
-                <Route path="/" element={<ChoseFigure/>}/>
-                <Route path="/board/:selectedFigure" element={<GameBoard />} />
-            </Routes>
-
-        </BrowserRouter>
-
-
-
-    </div>
-)
+                <Routes>
+                    <Route path="/" element={user ? <Navigate to="/profile" /> : <Login />} />
+                    <Route path="/register" element={<AddUserForm />} />
+                    <Route path="/profile" element={user ? <UserProfile /> : <Navigate to="/" />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
